a48dc16ea9fc14b10445a15f7f2ecdf609ac9dff
XueFeng <xfeng23@usfca.edu>
2018-10-19 16:58:04 -0700

    update.

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -1,22 +1,49 @@
 
 public class ArrayQueue implements Queue{
 
+	private Object[] arr; 
+	private int head; 
+	private int tail; 
+	
+	public ArrayQueue() {
+		arr = new Object[10]; 
+		head = 0; 
+		tail = 0; 
+	}
+	
 	@Override
 	public Object dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		Object obj = arr[head]; 
+		head++; 
+		head = head % arr.length; 
+		return obj; 
 	}
 
 	@Override
 	public void enqueue(Object item) {
-		// TODO Auto-generated method stub
+		
+		if ((tail + 1) % arr.length == head) {
+			grow_array(); 
+		}
+		arr[tail++] = item; 
+		tail = tail % arr.length; 
 		
 	}
 
 	@Override
 	public boolean empty() {
-		// TODO Auto-generated method stub
-		return false;
+		return head == tail; 
 	}
 
+	private void grow_array() {
+		Object[] newArr = new Object[arr.length * 2]; 
+		tail = 0; 
+		for (int i = head; i < arr.length + head; i++) {
+			newArr[tail++] = arr[i % arr.length]; 
+		}
+		tail -= 1; 
+		head = 0; 
+		arr = newArr; 
+	}
+	
 }
--- ArrayStack.java
+++ ArrayStack.java
@@ -1,28 +1,49 @@
 
 public class ArrayStack implements Stack{
 
+	private Object[] arr; 
+	private int top; 
+	
+	public Object[] getArr() {
+		return this.arr; 
+	}
+
+	public ArrayStack() {
+		top = -1; 
+		arr = new Object[10]; 
+	}
+
+	
 	@Override
 	public void push(Object item) {
-		// TODO Auto-generated method stub
+		if (top == arr.length - 1) {
+			grow_array(); 
+		} 
+		arr[++top] = item; 
 		
 	}
 
 	@Override
 	public Object pop() {
-		// TODO Auto-generated method stub
-		return null;
+		return arr[top--]; 
 	}
 
 	@Override
 	public Object peek() {
-		// TODO Auto-generated method stub
-		return null;
+		return arr[top]; 
 	}
 
 	@Override
 	public boolean empty() {
-		// TODO Auto-generated method stub
-		return false;
+		return top == -1; 
+	}
+	
+	private void grow_array() {
+		Object[] newArr = new Object[arr.length * 2]; 
+		for (int i = 0; i < arr.length; i++) {
+			newArr[i] = arr[i]; 
+		}
+		arr = newArr; 
 	}
 
 }
